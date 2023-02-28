aa481e58dd2e8990cf66810d46af6bcc2fad84cc
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-28 19:26:43 +0330

    add quantity &  increment button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import styles from './product.module.css'
+import React from "react";
+import styles from "./product.module.css";
 
 const Product = (props) => {
   return (
     <div className={styles.product}>
-      <p>product name : {props.name}</p>
-      <p>product price : {props.price}</p>
-      <button onClick={props.onDelete}>Delete</button>
+      <p>product name : {props.product.name}</p>
+      <p>product price : {props.product.price}</p>
+      <span className={styles.value}>{props.product.quantity}</span>
+      <button
+        onClick={props.onIncrement}
+        className={`${styles.button} ${styles.inc}`}
+      >
+        increment
+      </button>
+      <button onClick={props.onDelete} className={styles.button}>
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,16 +4,29 @@ import Product from "../Product/Product";
 class ProductList extends Component {
   state = {
     products: [
-      { title: "React.js", price: "99$", id: 1 },
-      { title: "Node.js", price: "89$", id: 2 },
-      { title: "Javascript", price: "79$", id: 3 },
+      { title: "React.js", price: "99 $", id: 1, quantity: 1 },
+      { title: "Node.js", price: "89 $", id: 2, quantity: 2 },
+      { title: "Javascript", price: "79 $", id: 3, quantity: 3 },
     ],
   };
 
   removeHandler = (id) => {
     console.log("clicked", id);
     const filterProduct = this.state.products.filter((p) => p.id !== id);
-    this.setState({ products : filterProduct})
+    this.setState({ products: filterProduct });
+  };
+
+  incrementHandler = (id) => {
+    // 1. id => ok
+    // 2. find selected item
+    // 3. add one to item quantity
+    // 4. setState()
+
+    const products = [...this.state.products];
+    const selectedItem = products.find((p) => p.id === id);
+    selectedItem.quantity++;
+    //  this.setState({products:products})
+    this.setState({ products });
   };
 
   render() {
@@ -22,10 +35,12 @@ class ProductList extends Component {
         {this.state.products.map((product, index) => {
           return (
             <Product
-              name={product.title}
-              price={product.price}
+              // name={product.title}
+              // price={product.price}
+              product={product}
               key={index}
               onDelete={() => this.removeHandler(product.id)}
+              onIncrement={() => this.incrementHandler(product.id)}
             />
           );
         })}
