1f6c26cb2f9aa1cd6aeacffe095dbb23b7b48890
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-03-23 11:40:57 +0430

    develop fullcomment component

--- src/components/FullComment/FullComment.js
+++ src/components/FullComment/FullComment.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import './fullComment.css'
+import React, { useEffect, useState } from "react";
+import "./fullComment.css";
+import axios from "axios";
 
-const FullComment = ({commentId}) => {
-  console.log(commentId)
+const FullComment = ({ commentId }) => {
+  const [comment, setComments] = useState(null);
+  console.log("comment:" + comment);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+      .then((response) => setComments(response.data))
+      .catch((error)=> console.log(error));
+  }, [commentId]);
+
+  if (!commentId) return <p>please select a comment !</p>;
   return (
-    <div className='fullComment'>
-        <p>name</p>
-        <p>email</p>
-        <p>body</p>
+    <div className="fullComment">
+      <p>{comment.name} </p>
+      <p>{comment.email} </p>
+      <p>{comment.body}</p>
     </div>
-  )
-}
+  );
+};
 
-export default FullComment
+export default FullComment;
