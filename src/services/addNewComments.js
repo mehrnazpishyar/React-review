fc567ecf6aebdf49b0cd616f606c3affc41f8ac7
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-04-01 21:43:29 +0330

    send token in header axios

--- src/services/addNewComments.js
+++ src/services/addNewComments.js
@@ -1,5 +1,11 @@
 import http from "./httpService";
 
 export function addNewComment (data) {
-    return http.post("/comments", data)
+    const token = "SECURE TOKEN"
+    const header ={
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    }
+    return http.post("/comments", data,header)
 }
