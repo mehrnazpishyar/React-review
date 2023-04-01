c75619cbff7606c9ae698ff5aea290727aefe311
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-04-01 21:31:52 +0330

    add axios-interceptor

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,28 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
 const http = {
   get: axios.get,
   put: axios.put,
